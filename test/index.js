13252fc7ca0e53dcb4c3f46c46225c0e3ff7b834
Clarkie <clarkie@concrete.cc>
2015-05-05 22:30:45 +0100

    add a basic plugin registration using lab to test

---
+++ test/index.js
@@ -0,0 +1,28 @@
+var Hapi = require('hapi');
+var Lab = require('lab');
+var Code = require('code');
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var describe = lab.describe;
+var it = lab.it;
+
+describe('Plugin', function () {
+
+    it('should register with hapi server', function (done) {
+
+		var server = new Hapi.Server();
+
+ 		var plugin = {
+            register: require('..'),
+            options: {}
+        };
+
+        server.register(plugin, function (err) {
+
+            expect(err).to.not.exist();
+
+        	return done();
+        });
+    });
+});
